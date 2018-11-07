da9cc0b63da899b5792dd88a5cee0f681d6db34c
Michele Ancheta <michelee.ancheta@gmail.com>
2018-11-06 19:04:32 -0500

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import TextLoop from 'react-text-loop'
 import Layout from '../components/ui/layout'
 import Button from '../components/ui/button'
 import Carousel from '../components/ui/carousel'
+import OurStoryImg from '../images/our-story.png'
 
 import ProductContent from '../components/index/productContent'
 import ApplicationsContent from '../components/index/applicationContent'
@@ -19,7 +20,14 @@ const IndexPage = () => (
       <ApplicationsContent />
       <div name="about" className="about section">
         <h2>We’re on a mission</h2>
-        <p>Lava Gel is a privately held company based in Las Vegas, NV.  We’re comprised of a diverse group of ambitious scientists, manufacturers, marketers, and industry veterans who are all dedicated to using mineral energy to transform the world we live in.  We believe bringing a safe, reliable heating source to everyday applications, from medical devices to food and beverage and everything in between is long overdue.  It’s this challenge that drives us every day.  And we wouldn’t have it any other way.</p>
+        <div className="inline-content">
+          <img src={OurStoryImg} alt="our-story" />
+          <p>Lava Gel is a privately held company based in Las Vegas, NV.  We’re comprised of a diverse
+            group of ambitious scientists, manufacturers, marketers, and industry veterans who
+            are all dedicated to using mineral energy to transform the world we live in. 
+            We believe bringing a safe, reliable heating source to everyday applications,
+            from medical devices to food and beverage and everything in between is long overdue. It’s this challenge that drives us every day.  And we wouldn’t have it any other way.</p>
+        </div>
       </div>
       <div name="contact" className="contact section">
         <h2>Contact Us</h2>
